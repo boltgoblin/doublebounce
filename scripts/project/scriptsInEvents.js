93d78eb171139b0563827b15bdd371d62e81fcce
Rafał Kowalski <rafal.kowalski@crankgoblin.com>
2024-12-31 11:10:49 +0100

    0.0.6

---
+++ scripts/project/scriptsInEvents.js
@@ -0,0 +1,19 @@
+
+
+
+const scriptsInEvents = {
+
+	async Egameplay_Event2_Act6(runtime, localVars)
+	{
+		document.body.style.backgroundColor = "#556eaa";
+	},
+
+	async Egameplay_Event7_Act1(runtime, localVars)
+	{
+		document.body.style.backgroundColor = "#283253";
+	}
+
+};
+
+self.C3.ScriptsInEvents = scriptsInEvents;
+
