c3cd804e364af8f0a491ee07329e762d68061f2f
Rafał Kowalski <rafal.kowalski@crankgoblin.com>
2025-02-10 12:32:57 +0100

    0.4

--- scripts/project/scriptsInEvents.js
+++ scripts/project/scriptsInEvents.js
@@ -3,9 +3,16 @@
 
 const scriptsInEvents = {
 
+	async Emenupadselection_Event28_Act1(runtime, localVars)
+	{
+		const ProcessedItemName = runtime.globalVars.ProcessedItemName
+		
+		runtime.callFunction("onItemHasMinted", ProcessedItemName)
+	},
+
 	async Egameplay_Event2_Act8(runtime, localVars)
 	{
-		document.body.style.backgroundColor = "#3c4e78";
+		document.body.style.backgroundColor = "#11141c";
 	},
 
 	async Egameplay_Event7_Act1(runtime, localVars)
